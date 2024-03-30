cc2d3989b731734e82e0659610e1adc258ff7b01
danielpfeifer02 <pfeifer.daniel@gmx.at>
2024-03-30 15:44:11 +0100

    test agianst reflect error

--- internal/ackhandler/sent_packet_handler.go
+++ internal/ackhandler/sent_packet_handler.go
@@ -3,7 +3,6 @@ package ackhandler
 import (
 	"errors"
 	"fmt"
-	"reflect"
 	"time"
 
 	"github.com/danielpfeifer02/quic-go-prio-packs/internal/congestion"
@@ -943,11 +942,12 @@ func (h *sentPacketHandler) SetPacketNumber(pn protocol.PacketNumber) {
 
 	gen := h.appDataPackets.pns
 
+	// TODO: add again
 	// Check type of generator
-	settableType := &settablePacketNumberGenerator{}
-	if reflect.TypeOf(gen) != reflect.TypeOf(settableType) {
-		fmt.Println("Trying to set packet number for generator of wrong type (sent_packet_handler.go)")
-		return
-	}
+	// settableType := &settablePacketNumberGenerator{}
+	// if reflect.TypeOf(gen) != reflect.TypeOf(settableType) {
+	// 	fmt.Println("Trying to set packet number for generator of wrong type (sent_packet_handler.go)")
+	// 	return
+	// }
 	gen.(*settablePacketNumberGenerator).SetPacketNumber(pn)
 }
